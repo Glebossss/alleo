13172cdc91e08b7938c93b56b33069ddc0a0e11e
Glebossss <gleb21.03.petmih@gmail.com>
2023-11-15 21:55:53 +0300

    third commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,16 @@ import logo from "../Images/logotype.jpg"
 function Header() {
   return (
     <nav>
-      <img
-        src={logo}
-        alt='logo'
-      />
-      <a href='#about'>О нас</a>
-      <a href='#partners'>Партнеры</a>
-      <a href='#services'>Преимущества</a>
-      <a href='#contacts'>Контакты</a>
+      <div>
+        <img
+          src={logo}
+          alt='logo'
+        />
+        <a href='#about'>О нас</a>
+        <a href='#partners'>Партнеры</a>
+        <a href='#services'>Преимущества</a>
+        <a href='#contacts'>Контакты</a>
+      </div>
     </nav>
   )
 }
